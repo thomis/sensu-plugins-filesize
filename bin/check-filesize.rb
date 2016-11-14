407daf7f635fda0cb99373c0ab25d381c3f83ae0
Steiner, Thomas <thomas.steiner@novartis.com>
2016-11-14 23:34:13 +0100

    + have size formatted

--- bin/check-filesize.rb
+++ bin/check-filesize.rb
@@ -51,11 +51,32 @@ class CheckFilesize < Sensu::Plugin::Check::CLI
 
       files_found.each do |file|
         file.strip!
-        buffer << "#{file} > #{File.size(file)}"
+        size = File.size(file)
+        buffer << "#{file} > #{format_size(size)}"
       end
 
       warning(buffer.join("\n"))
     end
+  end
+
+  private
+
+  def format_size(size)
+    conv = [ 'b', 'kb', 'mb', 'gb', 'tb', 'pb', 'eb' ];
+    scale = 1024;
+
+    ndx=1
+    if( size < 2*(scale**ndx)  ) then
+      return "#{(size)} #{conv[ndx-1]}"
+    end
+    size=size.to_f
+    [2,3,4,5,6,7].each do |ndx|
+      if( size < 2*(scale**ndx)  ) then
+        return "#{'%.3f' % (size/(scale**(ndx-1)))} #{conv[ndx-1]}"
+      end
+    end
+    ndx=7
+    return "#{'%.3f' % (size/(scale**(ndx-1)))} #{conv[ndx-1]}"
   end
 
 end
